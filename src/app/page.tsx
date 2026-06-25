ca89934d8e11ffd8cd41df5c64c9a9925f01729c
rahulprops <rahulhack912@gmail.com>
2026-06-25 14:19:54 +0530

    home content create

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import SearchBox from "@/components/home/SearchBox";
 import FrontentLayout from "@/components/layouts/FrontentLayout";
 import Navbar from "@/components/navbar/Navbar";
 
@@ -14,6 +15,28 @@ export default function Home() {
        {/* gradient */}
 
        <div className=" absolute inset-0 bg-linear-to-r from-slate-950/80 via-slate-900/50 to-transparent" />
+
+       {/* content */}
+
+       <div className=" relative z-10 w-full">
+        <div className=" mx-auto max-w-7xl  px-6 lg:px-12">
+          <div className=" max-w-3xl">
+            {/* badge */}
+            <div className=" mb-6 inline-flex items-center gap-2 rounded-full border border-white/10 bg-white/5 px-5 py-2 backdrop-blur-xl">
+            <div className=" w-2 h-2 rounded-full bg-primary" />
+            <span className=" text-sm font-medium tracking-wide text-white">Premium Real Estate Marketplace</span>
+            </div>
+
+            {/* heading */}
+              <h2 className=" text-4xl font-bold leading-tight text-white md:text-6xl lg:text-7xl">Find The Perfect Place To Call Home</h2>
+
+              <p className=" mt-6 max-w-2xl text-lg leading-relaxed text-white/70 "> Discover luxury apartments, modern homes, and premium Properties 
+              in the best locations around the world.  </p>
+              
+              <SearchBox/>
+              </div>
+        </div>
+       </div>
 
        </section>
     </FrontentLayout>
